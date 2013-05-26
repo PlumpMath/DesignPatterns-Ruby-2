199bae03d1af1fb026801077bbc2ed23944a286e
jeffa <jeffa@unlocalhost.com>
2013-05-26 15:30:03 -0400

    added TOC docs

--- lib/pattern/creational.rb
+++ lib/pattern/creational.rb
@@ -1,2 +1,65 @@
+# Pattern::Creational
+#   Table of Contents for Creational Design Patterns
+# 
+#  Abstract Factory
+#   Pattern::Creational::AbstractFactory
+# 
+#  Builder
+#   Pattern::Creational::Builder
+# 
+#  Factory Method
+#   Pattern::Creational::FactoryMethod
+# 
+#  Prototype
+#   Pattern::Creational::Prototype
+# 
+#  Singleton
+#   Pattern::Creational::Singleton
+# 
+#  SEE ALSO
+#   Pattern::Behavioral
+#   Pattern::Structural
+# 
+#  AUTHOR
+#   Jeffrey Hayes Anderson, info+rubygems at unlocalhost.com 
+# 
+#  LICENSE AND COPYRIGHT
+#   Copyright 2013 Jeffrey Hayes Anderson.
+# 
+#   This program is free software; you can redistribute it and/or modify it
+#   under the terms of the the Artistic License (2.0). You may obtain a
+#   copy of the full license at:
+# 
+#   http://www.perlfoundation.org/artistic_license_2_0
 class Pattern::Creational
 end
+
+# Any use, modification, and distribution of the Standard or Modified
+# Versions is governed by this Artistic License. By using, modifying or
+# distributing the Package, you accept this license. Do not use, modify,
+# or distribute the Package, if you do not accept this license.
+# 
+# If your Modified Version has been derived from a Modified Version made
+# by someone other than you, you are nevertheless required to ensure that
+# your Modified Version complies with the requirements of this license.
+# 
+# This license does not grant you the right to use any trademark, service
+# mark, tradename, or logo of the Copyright Holder.
+# 
+# This license includes the non-exclusive, worldwide, free-of-charge
+# patent license to make, have made, use, offer to sell, sell, import and
+# otherwise transfer the Package with respect to any patent claims
+# licensable by the Copyright Holder that are necessarily infringed by the
+# Package. If you institute patent litigation (including a cross-claim or
+# counterclaim) against any party alleging that the Package constitutes
+# direct or contributory patent infringement, then this Artistic License
+# to you shall terminate on the date that such litigation is filed.
+# 
+# Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+# AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+# YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+# CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+# CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
